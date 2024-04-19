b49e4d68ad1139e9a16cddbd15f29dbd7dd97fd9
Claudius Appel <151634114+Claudius-Appel@users.noreply.github.com>
2024-04-19 14:22:10 +0200

    `execute_single()`: add static results NAME/DATE/DIMENSIONS

--- R/execute_single.R
+++ R/execute_single.R
@@ -1,4 +1,16 @@
 execute_single <- function(file, input, DATA, DEBUGKEYS, FLAGS) {
     #### INIT RESULTS-OBJECT ####
     results_object <- prepare_resultsObject(input,DATA,DEBUGKEYS)
+    # create a results-row to be merged into the `results_object`
+    current_results <- data.frame(matrix(NA, nrow = 1, ncol = length(names(results_object))))
+    colnames(current_results) <- names(results_object)
+    #TODO: decide if these paths must be sanitized still?
+    current_results$full_path <- file
+    current_results$image_name <- basename(file)
+    ## DATE_OF_ANALYSIS
+    current_results$date_of_analysis <- input$date_of_image_shooting
+    ## IMAGE DIMENSIONS
+    image_dimensions <- as.integer(duflor.get_image_dimensions(file))
+    current_results$image_width <- image_dimensions[[1]]
+    current_results$image_height <- image_dimensions[[2]]
 }
