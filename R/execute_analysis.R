0e3afd08f8f8a7c54f3215f251b4b37d560be52a
Claudius Appel <151634114+Claudius-Appel@users.noreply.github.com>
2024-04-19 13:03:16 +0200

    setup and tear-down parallelisation if selected

--- R/execute_analysis.R
+++ R/execute_analysis.R
@@ -1,3 +1,26 @@
 execute_analysis <- function(input, DATA, DEBUGKEYS) {
+    #### SETUP PARALLELISATION ####
+    if (input$parallel_cores > 1) {
+        if (.Platform$OS.type == "windows") {
+            cluster_type <- "PSOCK"
+        } else {
+            cluster_type <- "FORK"
+        }
+        if (getDoParRegistered()) {
+            # shut down existing cluster first (?)
+            shutdown_parallel()
+        }
+        setup_parallel(input$parallel_cores, cluster_type)
+    } else {
+        if (getDoParRegistered()) {
+            # shut down existing cluster first (?)
+            shutdown_parallel()
+        }
+    }
 
+    #### TEAR DOWN PARALLELISATION ####
+    if (getDoParRegistered()) {
+        # finally, shutdown the cluster if work was performed in parallel
+        shutdown_parallel()
+    }
 }
