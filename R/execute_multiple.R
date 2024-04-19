025e9388eea44a65c812ca9e0514c2bdc9d63bcf
Claudius Appel <151634114+Claudius-Appel@users.noreply.github.com>
2024-04-19 13:56:05 +0200

    initialise results-object

--- R/execute_multiple.R
+++ R/execute_multiple.R
@@ -1,3 +1,5 @@
 execute_multiple <- function(files, input, DATA, DEBUGKEYS, FLAGS) {
+    #### INIT RESULTS-OBJECT ####
+    results_object <- prepare_resultsObject(input,DATA,DEBUGKEYS)
 
 }
--- R/execute_single.R
+++ R/execute_single.R
@@ -1,3 +1,4 @@
 execute_single <- function(file, input, DATA, DEBUGKEYS, FLAGS) {
-
+    #### INIT RESULTS-OBJECT ####
+    results_object <- prepare_resultsObject(input,DATA,DEBUGKEYS)
 }
