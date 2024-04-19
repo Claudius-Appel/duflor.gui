a473933f609bf43798f4086c38a20f4357381ff0
Claudius Appel <151634114+Claudius-Appel@users.noreply.github.com>
2024-04-19 14:12:13 +0200

    `execute_multiple()`: iterate over images when processing them

--- R/execute_multiple.R
+++ R/execute_multiple.R
@@ -1,5 +1,8 @@
 execute_multiple <- function(files, input, DATA, DEBUGKEYS, FLAGS) {
     #### INIT RESULTS-OBJECT ####
     results_object <- prepare_resultsObject(input,DATA,DEBUGKEYS)
+    #### ITERATE OVER IMAGES ####
+    for (index in files$count) {
 
+    }
 }
