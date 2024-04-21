669796427fc214560065f3ab578606c296d8db2d
Claudius Appel <151634114+Claudius-Appel@users.noreply.github.com>
2024-04-21 13:01:38 +0200

    formatting

--- R/prepare_resultsObject.R
+++ R/prepare_resultsObject.R
@@ -19,9 +19,7 @@
 #' @return results-csv-framework.
 #' @keywords internal
 #'
-prepare_resultsObject <- function(input,DATA,DEBUGKEYS) {
-
-
+prepare_resultsObject <- function(input, DATA, DEBUGKEYS) {
     # static fields - always present
     column_names <- c("full_path", "image_date", "image_name", "image_width", "image_height")
 
@@ -32,9 +30,8 @@ prepare_resultsObject <- function(input,DATA,DEBUGKEYS) {
                           paste0(spectrum, "_fraction"))
     }
 
-    # init a NA-filled df,
+    # init a NA-filled df, assign the column-names and return it out of the func.
     df <- data.frame(matrix(NA, nrow = 0, ncol = length(column_names)))
     colnames(df) <- column_names
-    # warning(simpleWarning("function not implemented: must prepare the csv-struct to be used for collecting results, and results will then have to be inserted each iter by function 'modify_resultsObject(resultsObject,results*)"))
     return(df)
 }
--- R/validate_croppings.R
+++ R/validate_croppings.R
@@ -1,8 +1,3 @@
-validate_croppings <-
-    function(image_dimensions,
-             crop_left,
-             crop_right,
-             crop_top,
-             crop_bottom) {
+validate_croppings <- function(image_dimensions, x0, x1, y0, y1) {
         simpleWarning("this function is not yet implemented")
     }
