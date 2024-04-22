f500972e8fabed4be2647fdbe6559cfb28b42470
Claudius Appel <151634114+Claudius-Appel@users.noreply.github.com>
2024-04-22 19:57:15 +0200

    retrieve filepath from `DATA$results$results` when viewing results of a single-analysis, to avoid frameshift, closes #10

--- R/app.R
+++ R/app.R
@@ -590,7 +590,11 @@ duflor_gui <- function() {
                 )
                 return()
             }
-            file <- DATA$r__tbl_dir_files$images_filtered[[input$tbl_results_filtered_rows_selected]]
+            if (FLAGS$analyse_single_image) {
+                file <- DATA$results$results$full_path
+            } else {
+                file <- DATA$r__tbl_dir_files$images_filtered[[input$tbl_results_filtered_rows_selected]]
+            }
             image_dimensions <- as.integer(get_image_dimensions(file))
             ## LOAD IMAGE
             if (is.na(DATA$last_masked_image) || (DATA$last_masked_image!=file)) {
