3072112301c0ce42f007017e7c4b1952bd5cc253
Claudius Appel <151634114+Claudius-Appel@users.noreply.github.com>
2024-05-06 21:18:10 +0200

    `duflor_gui()`: implement `tryCatch()`-routine for callback `input$dev_pass`

--- R/app.R
+++ R/app.R
@@ -395,7 +395,28 @@ duflor_gui <- function() {
         hide("PARALLEL_PANEL")
         #### DEV TOGGLES ####
         observeEvent(input$dev_pass, {
-            DEBUGKEYS <- dev_key_handler(input, DATA, DEBUGKEYS, session, use_logical_cores)
+            input_mirror <- input ## mirror input so that the error-trycatch can pass it to save_state
+            tryCatch({
+                DEBUGKEYS <- dev_key_handler(input, DATA, DEBUGKEYS, session, use_logical_cores)
+            }, error = function(e) {
+                DATA$stacktrace = traceback(1, 1)
+                error_state_path <- save_error_state(
+                    input_mirror,
+                    DATA = DATA,
+                    DEBUGKEYS = DEBUGKEYS,
+                    FLAGS = FLAGS,
+                    volumes = getVolumes(),
+                    error = e,
+                    errordir_path = DATA$folder_path,
+                    erroneous_callback = "dev_pass"
+                )
+                showNotification(
+                    ui = str_c("Error occured during callback 'input$dev_pass'. The configuration which triggered this error was stored to '",error_state_path,"'."),
+                    id = "error_state_generated.done",
+                    duration = NULL,
+                    type = "error"
+                )
+            })
         })
         #### SETUP PARALLELISATION ####
         observeEvent(input$open_parallelPanel, {
